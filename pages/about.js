ce92c155064d821ec2311cc31608145e3b68d834
Justin Newton <justinmnewton25@gmail.com>
2023-08-09 20:25:39 -0500

    update about page

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,10 @@ export default function About() {
 			<h1>About</h1>
 			<p>My name is justin newton</p>
 			<p> / add img here / </p>
-			<p></p>
+			<p>
+				my current role is a software developer intern for RSS at WR
+				Berkley
+			</p>
 			<h2>
 				<Link href="/">Back to home</Link>
 			</h2>
